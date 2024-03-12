1809d246c5ea48f0380143353705a4083365bd9b
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-03-12 20:36:07 -0300

    feito

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿Console.WriteLine("Veracruz (Mc Kevin)");
+
+Console.WriteLine("E aê, DJ Jorginho?");
+Console.WriteLine("Tá ligado, né pai?");
+
+Console.WriteLine("De Vera Cruz eu passei");
+Console.WriteLine("Ela ficou descontrolada");
+Console.WriteLine("E pedi àquela outra mais safada");
+Console.WriteLine("E eu com cara tipo de quem não quer nada");
+
+Console.WriteLine("Mas quem vê cara, não vê coracão");
+Console.WriteLine("Entre quatro paredes pela escuridão");
+Console.WriteLine("Caiu na lábia e provou minha intenção");
+
+Console.WriteLine("Não te esqueci");
+Console.WriteLine("E desde o tempo de criança");
+Console.WriteLine("Não era nada na infância");
+Console.WriteLine("Tenho certeza que hoje o ronco é a esperança");
+
+Console.WriteLine("Da 66 alcançar");
+Console.WriteLine("O teus maior objetivo");
+Console.WriteLine("Na garupa, uma mina ou teu amigo");
+Console.WriteLine("Todo de grife, é o traje desse menino");
+
+Console.WriteLine("Com o céu azul");
+Console.WriteLine("O destino foi uma tal de Ilha Bela");
+Console.WriteLine("Só de avistar, todas as mulheres são belas");
+Console.WriteLine("Disse pra ela que eu sou a fera");
+
+Console.WriteLine("Acho que é lá o castelo da Cinderela");
+Console.WriteLine("Acho que é lá o castelo da Cinderela");
+Console.WriteLine("Acho que é lá o castelo da Cinderela");
+Console.WriteLine("Acho que é lá o castelo da Cinderela");
+
+Console.WriteLine("E aêh, DJ Jorginho?");
+Console.WriteLine("Tá ligado, né pai?");
